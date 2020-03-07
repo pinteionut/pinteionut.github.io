6b310f325e632c3633ba25388d1c838b593ce299
Ionut Pinte <pinte.ionut.andrei@gmail.com>
2020-03-07 19:56:20 +0200

    add-university-page
    Added University Page
    Added PW L2 T1

---
+++ pages/university/programare_web/lab_2/tema_1/default_books.js
@@ -0,0 +1,52 @@
+let defaultBooks = [
+    {
+        chapter: 'HTML',
+        title: 'Beginning HTML and CSS',
+        imageLink: 'https://images-na.ssl-images-amazon.com/images/I/51%2BusxoxCSL._SX258_BO1,204,203,200_.jpg',
+        author: 'Rob Larsen',
+        publisher: 'Wrox',
+        year: '2013',
+        link: 'https://www.amazon.com/Beginning-HTML-CSS-Rob-Larsen/dp/1118340183',
+        price: '31.49'
+    },
+    {
+        chapter: 'HTML',
+        title: 'Head First HTML and CSS, 2nd Edition',
+        imageLink: 'https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcSRpWq89yXCdYRkoSF0WudVDsfG0-6HTQxAjfofuj0cWByk00IM',
+        author: 'E. Robson, E. Freeman',
+        publisher: "O'Reilly Media",
+        year: '2012',
+        link: 'https://www.amazon.com/Head-First-HTML-CSS-Standards-Based/dp/0596159900',
+        price: '28.99'
+    },
+    {   
+        chapter: 'HTML',
+        title: 'Build Your Own Web Site, 2nd Edition',
+        imageLink: 'https://images-na.ssl-images-amazon.com/images/I/51eXTl3j44L.jpg',
+        author: 'Ian LLoyd',
+        publisher: 'SitePoint',
+        year: '2008',
+        link: 'https://www.amazon.com/Build-Your-Site-Right-Using/dp/0980455278',
+        price: '8.86'
+    },
+    {   
+        chapter: 'PHP',
+        title: 'Jump Start PHP',
+        imageLink: 'https://images-na.ssl-images-amazon.com/images/I/51GGwq0cmOL._SX258_BO1,204,203,200_.jpg',
+        author: 'Callum Hopkins',
+        publisher: 'SitePoint',
+        year: '2013',
+        link: 'https://www.amazon.com/Jump-Start-PHP-Speed-Weekend/dp/0987467409',
+        price: '1.97'
+    },
+    {   
+        chapter: 'PHP',
+        title: 'Learning PHP, MySQL, JavaScript, CSS & HTML5, 3rd Edtition',
+        imageLink: 'https://covers.oreillystatic.com/images/0636920033004/lrg.jpg',
+        author: 'Robin Nixon',
+        publisher: "O'Reilly Media",
+        year: '2014',
+        link: 'https://www.amazon.com/Learning-PHP-MySQL-JavaScript-Javascript/dp/1491978910/ref=sr_1_fkmr1_1?keywords=learning+php%2C+mysql%2C+javascript%2C+css+%26+html5%2C+3rd+edition&qid=1583573493&s=books&sr=1-1-fkmr1',
+        price: '24.49' 
+    }
+];
